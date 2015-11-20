c125cdb1c3310445ae87872e1715f79c5e39fd69
Fernando Souza <fernando.souza@liferay.com>
2015-11-20 02:13:04 -0300

    Runing without graceful degradation

--- src/Rating.js
+++ src/Rating.js
@@ -1,8 +1,232 @@
 'use strict';
 
 import RatingBase from './Rating.soy';
+import core from 'bower:metal/src/core';
 
 class Rating extends RatingBase {
+	/**
+	 * @inheritDoc
+	 */
+	constructor(opt_config) {
+		super(opt_config);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	attached() {
+		this.delegate('mouseover', 'a', this.handleMouseOverEvent.bind(this));
+		this.delegate('click', 'a', this.handleClickEvent.bind(this));
+		this.on('mouseout', this.handleMouseOutEvent.bind(this));
+	}
+
+	/**
+	 * Handles click event
+	 * @param {Event} event
+	 * @protected
+	 */
+	handleClickEvent(event) {
+		if (!this.disabled) {
+			var index = parseInt(event.target.dataset.index, 10);
+			var selectedItem = this.items[index];
+			var rate = selectedItem.value;
+
+			if (rate === this.previousRate && this.canReset) {
+				this._reset();
+			}
+			else {
+				this.selectedItem = selectedItem;
+				this.selectedIndex = index;
+			}
+		}
+	}
+
+	_reset() {
+		this.rate = null;
+		this.selectedItem = null;
+		this.selectedIndex = -1;
+	}
+
+	/**
+	 * Handles mouseleave event
+	 * @protected
+	 */
+	handleMouseOutEvent() {
+		this.currentMouseTarget = undefined;
+		this.onStartIndex = this.selectedIndex;
+	}
+
+	/**
+	 * Handles mouseover event
+	 * @param {Event} event
+	 * @protected
+	 */
+	handleMouseOverEvent(event) {
+		if (!this.disabled) {
+			var index = Number.parseInt(event.target.dataset.index, 10);
+
+			if (this.currentMouseTarget !== index) {
+				this.onStartIndex = index;
+			}
+
+			this.currentMouseTarget = index;
+		}
+	}
+
+	syncRate(rate) {
+		this.previousRate = rate;
+	}
+
+	syncSelectedItem(item) {
+		if (item) {
+			this.rate = item.value;
+		}
+	}
+
+	syncSelectedIndex(index) {
+		this.onStartIndex = index;
+	}
 }
+
+Rating.ATTRS = {
+
+	/**
+	 * Block or unblock rating functionality
+	 * @type {?boolean}
+	 * @default false
+	 */
+	cssClasses: {
+		value: {
+			off: 'glyphicon glyphicon-star-empty',
+			on: 'glyphicon glyphicon-star'
+		}
+	},
+
+	/**
+	 * Block or unblock rating functionality
+	 * @type {?boolean}
+	 * @default false
+	 */
+	disabled: {
+		value: false,
+		validator: core.isBoolean
+	},
+
+	/**
+	 * The current rate
+	 * @type {?number}
+	 * @default 0
+	 */
+	rate: {
+		value: null,
+		setter: function(rate) {
+			return (this.disabled) ? this.previousRate : rate;
+		}
+	},
+
+	/**
+	 * The last rate selected
+	 * @type {?number}
+	 * @default 0
+	 */
+	previousRate: {
+		value: 0
+	},
+
+	/**
+	 * The current highlighted index
+	 * @type {number}
+	 * @default -1
+	 */
+	selectedIndex: {
+		validator: core.isNumber,
+		value: -1
+	},
+
+	/**
+	 * The current item object
+	 * @type {object}
+	 * @default null
+	 */
+	selectedItem: {
+		validator: core.isObject,
+		value: null,
+		valueFn: function() {
+			return this.items[this.selectedIndex];
+		} 
+	},
+
+	/**
+	 * The arry of items. It will be automaticly created if its value isn't a array.
+	 * @type {array}
+	 * @default 5
+	 */
+	items: {
+		validator: function (value) {
+			return (core.isNumber(value) || value.length)
+		},
+
+		value: [],
+
+		setter: function(value) {
+			var items = [];
+
+			if (value && value.length) {
+				return value;
+			}
+			else {
+				value = value;
+
+				if (!core.isNumber(value)) {
+					value = 5;
+				}
+
+				for (var i = 0; i < value; i++) {
+					items.push({
+						title: '',
+						value: i + 1
+					});
+				}
+
+				return items;
+			}
+		}
+	},
+
+	/**
+     * If `true` could be reseted
+     * (i.e., have no values selected).
+     * @type {boolean}
+     * @default true
+     */
+    canReset: {
+        value: true,
+        validator: core.isBoolean
+    },
+
+    /**
+     * Label to be displayed with the Rating elements.
+     *
+     * @attribute label
+     * @type {string}
+     * @default ''
+     */
+	label: {
+		value: '',
+		validator: core.isString
+	},
+
+	/**
+     * Name of the hidden input.
+     *
+     * @attribute inputHiddenName
+     * @type {string}
+     * @default 'rate'
+     */
+	inputHiddenName: {
+		value: 'rate',
+		validator: core.isString	
+	}
+};
 
 export default Rating;
--- src/Rating.soy.js
+++ src/Rating.soy.js
@@ -1,6 +1,8 @@
 /* jshint ignore:start */
-import Component from 'metal-component';
-import { SoyAop, SoyRenderer, SoyTemplates } from 'metal-soy';
+import Component from 'bower:metal/src/component/Component';
+import SoyAop from 'bower:metal/src/soy/SoyAop';
+import SoyRenderer from 'bower:metal/src/soy/SoyRenderer';
+import SoyTemplates from 'bower:metal/src/soy/SoyTemplates';
 var Templates = SoyTemplates.get();
 // This file was automatically generated from Rating.soy.
 // Please don't edit this file by hand.
@@ -20,13 +22,69 @@ if (typeof Templates.Rating == 'undefined') { Templates.Rating = {}; }
  * @suppress {checkTypes}
  */
 Templates.Rating.render = function(opt_data, opt_ignored, opt_ijData) {
-  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '" class="rating component' + soy.$$escapeHtmlAttribute(opt_data.elementClasses ? ' ' + opt_data.elementClasses : '') + '"></div>');
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '" class="rating component' + soy.$$escapeHtmlAttribute(opt_data.elementClasses ? ' ' + opt_data.elementClasses : '') + '">' + ((opt_data.label) ? Templates.Rating.rateLabel(opt_data, null, opt_ijData) : '') + Templates.Rating.rateItems(opt_data, null, opt_ijData) + Templates.Rating.rateInput(opt_data, null, opt_ijData) + '</div>');
 };
 if (goog.DEBUG) {
   Templates.Rating.render.soyTemplateName = 'Templates.Rating.render';
 }
 
-Templates.Rating.render.params = ["id"];
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @param {Object.<string, *>=} opt_ijData
+ * @return {!soydata.SanitizedHtml}
+ * @suppress {checkTypes}
+ */
+Templates.Rating.rateLabel = function(opt_data, opt_ignored, opt_ijData) {
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<span id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-rateLabel" class="rate-label">' + soy.$$escapeHtml(opt_data.label) + '</span>');
+};
+if (goog.DEBUG) {
+  Templates.Rating.rateLabel.soyTemplateName = 'Templates.Rating.rateLabel';
+}
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @param {Object.<string, *>=} opt_ijData
+ * @return {!soydata.SanitizedHtml}
+ * @suppress {checkTypes}
+ */
+Templates.Rating.rateItems = function(opt_data, opt_ignored, opt_ijData) {
+  var output = '<div id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-rateItems" class="rating-items">';
+  var itemList24 = opt_data.items;
+  var itemListLen24 = itemList24.length;
+  for (var itemIndex24 = 0; itemIndex24 < itemListLen24; itemIndex24++) {
+    var itemData24 = itemList24[itemIndex24];
+    output += '<a data-index="' + soy.$$escapeHtmlAttribute(itemIndex24) + '" title="' + soy.$$escapeHtmlAttribute(itemData24.title) + '" class="' + soy.$$escapeHtmlAttribute(itemIndex24 <= opt_data.onStartIndex ? opt_data.cssClasses.on : opt_data.cssClasses.off) + '"></a>';
+  }
+  output += '</div>';
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml(output);
+};
+if (goog.DEBUG) {
+  Templates.Rating.rateItems.soyTemplateName = 'Templates.Rating.rateItems';
+}
+
+
+/**
+ * @param {Object.<string, *>=} opt_data
+ * @param {(null|undefined)=} opt_ignored
+ * @param {Object.<string, *>=} opt_ijData
+ * @return {!soydata.SanitizedHtml}
+ * @suppress {checkTypes}
+ */
+Templates.Rating.rateInput = function(opt_data, opt_ignored, opt_ijData) {
+  return soydata.VERY_UNSAFE.ordainSanitizedHtml('<input id="' + soy.$$escapeHtmlAttribute(opt_data.id) + '-rateInput" type="text" name="' + soy.$$escapeHtmlAttribute(opt_data.inputHiddenName) + '" value="' + soy.$$escapeHtmlAttribute(opt_data.rate) + '"  />');
+};
+if (goog.DEBUG) {
+  Templates.Rating.rateInput.soyTemplateName = 'Templates.Rating.rateInput';
+}
+
+Templates.Rating.render.params = ["elementClasses","label","id"];
+Templates.Rating.rateLabel.params = ["label","id"];
+Templates.Rating.rateItems.params = ["cssClasses","onStartIndex","items","id"];
+Templates.Rating.rateInput.params = ["rate","inputHiddenName","id"];
 
 class Rating extends Component {}
 Rating.RENDERER = SoyRenderer;
